654bbc9c7228f323128c049e6230a50abf1696ca
ItsIvanPsk <ivanfigueredo.et@gmail.com>
2022-10-14 16:11:03 +0200

    Threads de Comunicacio Release v1.0

--- app/src/main/java/com/example/threadsdecomunicacio/MainActivity.java
+++ app/src/main/java/com/example/threadsdecomunicacio/MainActivity.java
@@ -1,14 +1,134 @@
 package com.example.threadsdecomunicacio;
 
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
+
+    String str = "";
+    String error = "";
+
+    ArrayList<String> strs = new ArrayList<>();
+    ArrayAdapter<String> adapter;
+
+    Runnable r = new Runnable() {
+        @Override
+        public void run() { }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.main__button__getUrl);
+
+        // Inicialitzem l'ArrayAdapter amb el layout pertinent
+        adapter = new ArrayAdapter<String>( this, R.layout.item_list, strs )
+        {
+            @Override
+            public View getView(int pos, View convertView, ViewGroup container)
+            {
+                // getView ens construeix el layout i hi "pinta" els valors de l'element en la posició pos
+                if( convertView==null ) {
+                    // inicialitzem l'element la View amb el seu layout
+                    convertView = getLayoutInflater().inflate(R.layout.item_list, container, false);
+                }
+                // "Pintem" valors (també quan es refresca)
+                ((TextView) convertView.findViewById(R.id.list_label)).setText(strs.get(pos));
+                return convertView;
+            }
+
+        };
+
+        ListView lv = (ListView) findViewById(R.id.main__listview);
+        lv.setAdapter(adapter);
+
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Handler handler = new Handler(Looper.getMainLooper());
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                executor.execute(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        str = getDataFromUrl("https://api.myip.com");
+                        strs.add(str);
+                        final Handler handler1 = new Handler(Looper.getMainLooper());
+                        handler1.postDelayed(r, 1000);
+
+                        handler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                adapter.notifyDataSetChanged();
+                            }
+                        });
+                    }
+                });
+            }
+        });
+
+
+    }
+
+    private String getDataFromUrl(String demoIdUrl) {
+
+        String result = null;
+        int resCode;
+        InputStream in;
+        try {
+            URL url = new URL(demoIdUrl);
+            URLConnection urlConn = url.openConnection();
+
+            HttpsURLConnection httpsConn = (HttpsURLConnection) urlConn;
+            httpsConn.setAllowUserInteraction(false);
+            httpsConn.setInstanceFollowRedirects(true);
+            httpsConn.setRequestMethod("GET");
+            httpsConn.connect();
+            resCode = httpsConn.getResponseCode();
+
+            if (resCode == HttpURLConnection.HTTP_OK) {
+                in = httpsConn.getInputStream();
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(
+                        in, "iso-8859-1"), 8);
+                StringBuilder sb = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line).append("\n");
+                }
+                in.close();
+                result = sb.toString();
+            } else {
+                error += resCode;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 }
